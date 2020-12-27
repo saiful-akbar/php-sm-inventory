476c64ad27b59f22ba38e0f68866e042e174bd26
Saiful Akbar <saifulakbar.app@gmail.com>
2020-12-27 21:21:33 +0700

    Update init.php

--- app/init.php
+++ app/init.php
@@ -4,8 +4,7 @@ require_once __DIR__.'/config/config.php';
 
 /**
  * Fungsi untuk menjalankan semua class pada folder core
- * NB: Pastikan nama file disesuaikan dengan nama class nya
- * Jadikan 1 file untuk 1 class
+ * NB: Pastikan nama file disesuaikan dengan nama class-nya & jadikan 1 file untuk 1 class
  */
 spl_autoload_register(function($class) {
 	require_once __DIR__.'/core/'.$class.'.php';
